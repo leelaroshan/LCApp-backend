219b0d0b83d7cfd010e137fc53d86a32fb9a6008
Leelavathi Gade <leelaroshangade@gmail.com>
2021-10-06 13:13:52 +0200

    cors added

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 const io = require("socket.io")(server,{
     cors:{
-        origin: "http://localhost:3000",
+        origin: "https://keen-meitner-550697.netlify.app/",
         method: ["GET","POST"]
     }
 })
